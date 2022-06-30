3139a16133d33ffd90aa94cbceb02567af39d35d
Gabriel Coelho <gabrielcoelhodev1995@gmail.com>
2022-06-30 09:31:52 -0300

    alguns exercicios usando appenchild concluidos

--- script.js
+++ script.js
@@ -24,3 +24,47 @@
 
  // 8. Agora acesse o terceiroFilho a partir de pai.
  const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
+
+
+ /////////////////////////////////////////////
+
+
+ // passos para adicionar tags dentro de um elemento
+ //pegar o elemento que eu quero adicionar dentro
+let pegaSection = document.querySelector("#pai")
+//criar esse elemento
+let novaSection = document.createElement("section")
+novaSection.className = "irmaoDoElementoOndeVoceEsta"
+console.log(novaSection)
+// adicionar o elemento
+pegaSection.appendChild(novaSection)
+//escreve algo dentro dele
+let escreveTextoNaSection = document.createTextNode("irmao elementoOndeVoceEsta");
+novaSection.appendChild(escreveTextoNaSection)
+
+
+//Criar um filho para elementoOndeVoceEsta
+let pegaPaiOndeVoceEsta = document.querySelector("#elementoOndeVoceEsta");
+let filhoDoElementoOndeVoceEsta = document.createElement("section");
+filhoDoElementoOndeVoceEsta.id = "TerceiroFilhoDoElementoOndeVoceEsta"
+pegaPaiOndeVoceEsta.appendChild(filhoDoElementoOndeVoceEsta)
+
+let textoTerceiroFilho = document.createTextNode("Sou terceiro filho")
+filhoDoElementoOndeVoceEsta.appendChild(textoTerceiroFilho)
+
+
+//Criar um filho para primeiroFilhoDoFilho
+
+let filhoDofilho = document.querySelector("#primeiroFilhoDoFilho")
+let nasceuOutroFilhoDoFilho = document.createElement("div")
+nasceuOutroFilhoDoFilho.id = "Oipapaieunasci"
+filhoDofilho.appendChild(nasceuOutroFilhoDoFilho)
+
+let textoNoFilhoQueNasceu = document.createTextNode("Nasci papai")
+nasceuOutroFilhoDoFilho.appendChild(textoNoFilhoQueNasceu)
+  
+  /////////////////////////////////////////////
+
+
+  //A partir desse filho criado, acesse terceiroFilho.
+  const terceiroFilho3 = Oipapaieunasci.lastElementChild.previousElementSibling;
